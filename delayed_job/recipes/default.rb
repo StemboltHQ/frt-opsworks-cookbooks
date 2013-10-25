8e2f67e01857fc3491362e24ae262cca8c73be00
John Hawthorn <john@freerunningtechnologies.com>
2013-10-25 11:35:10 -0700

    allow multiple delayed_job processes
    This allows specifying multiple delayed job instances per application,
    and allows per application and per-worker customization.
    
    {
      "deploy": {
        "my_app": {
          "delayed_job": [
            {
              "identifier": "regular",
              "timeout": 60
            },
            {
              "identifier": "priority",
              "options": "--min-priority 10",
              "timeout": 30
            }
          ]
        }
      }
    }
    
    Change-Id: I9699a500e6c231b37beabd951aaed2773a10b74d

--- delayed_job/recipes/default.rb
+++ delayed_job/recipes/default.rb
@@ -12,6 +12,27 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
+  global = node[:delayed_job] || {}
+
+  if deploy[:delayed_job].is_a?(Array)
+    index = 0
+    workers = deploy[:delayed_job].map do |dj|
+      index += 1
+      identifier = dj[:identifier] || index
+      {
+        :timeout => dj[:timeout] || global[:timeout],
+        :bin => dj[:bin] || global[:bin],
+        :identifier => identifier,
+        :suffix => "_#{identifier}",
+        :options => dj[:options] || global[:options]
+      }
+    end
+  else
+    # For backwards compatability with existing json
+    # :delayed_job => true
+    workers = [global]
+  end
+
   template "/etc/monit.d/#{application}_delayed_job.monitrc" do
     source 'delayed_job.monitrc.erb'
     owner 'root'
@@ -23,7 +44,7 @@ node[:deploy].each do |application, deploy|
       :dir => deploy[:deploy_to],
       :user => deploy[:user],
       :group => deploy[:group],
-      :timeout => node[:delayed_job][:timeout]
+      :workers => workers
     )
     only_if { deploy[:delayed_job] }
   end
