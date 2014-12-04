1fd997cf54cfe0530fb037bb84acab5b4279047e
John Hawthorn <john@freerunningtechnologies.com>
2014-12-04 13:40:55 -0800

    Detect existance of deploy::rails recipe

--- deploy/recipes/rails-noserver.rb
+++ deploy/recipes/rails-noserver.rb
@@ -1,7 +1,8 @@
-include_recipe 'deploy::rails'
+unless node.recipes.include?('deploy::rails')
+  include_recipe 'deploy::rails'
 
-node[:deploy].each do |application, deploy|
-  r = resources(template: "#{deploy[:deploy_to]}/shared/scripts/unicorn")
-  r.source('unicorn.service.disabled.erb')
+  node[:deploy].each do |application, deploy|
+    r = resources(template: "#{deploy[:deploy_to]}/shared/scripts/unicorn")
+    r.source('unicorn.service.disabled.erb')
+  end
 end
-
