df9e43ace5ede1989cc8e93df0c3053e88cc35c8
Adam Mueller <adam@freerunningtech.com>
2014-12-08 14:01:32 -0800

    Add image/svg+xml to gzip types.

---
+++ nginx/attributes/customize.rb
@@ -0,0 +1,11 @@
+override[:nginx][:gzip_types] = [
+  "application/x-javascript",
+  "application/xhtml+xml",
+  "application/xml",
+  "application/xml+rss",
+  "text/css",
+  "text/javascript",
+  "text/plain",
+  "text/xml",
+  "image/svg+xml"
+]
