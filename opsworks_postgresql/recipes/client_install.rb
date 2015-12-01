e8d7d969dfb78c5c1c4cfb3ee45f71a013467d3f
Alex Blackie <alex@alexblackie.com>
2015-11-30 16:12:33 -0800

    Use PostgreSQL 9.4 client on Amazon Linux
    Otherwise it not only fails every Chef setup, but also installs
    `postgresql8-devel` which is *incredibly* out of date and useless.

--- opsworks_postgresql/recipes/client_install.rb
+++ opsworks_postgresql/recipes/client_install.rb
@@ -1,6 +1,7 @@
 package "postgresql-devel" do
   package_name value_for_platform(
-    ["centos","redhat","fedora","amazon"] => {"default" => "postgresql-devel"},
+    ["centos","redhat","fedora"] => {"default" => "postgresql-devel"},
+    "amazon" => {"default" => "postgresql94-devel"},
     "ubuntu" => {"default" => "libpq-dev"}
   )
 
@@ -9,8 +10,10 @@ end
 
 package "postgresql-client" do
   package_name value_for_platform(
-    ["centos","redhat","fedora","amazon"] => {"default" => "postgresql"},
+    ["centos","redhat","fedora"] => {"default" => "postgresql"},
+    "amazon" => {"default" => "postgresql94"},
     "default" => "postgresql-client"
   )
+
   action :install
 end
