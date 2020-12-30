8b161093dbf82cfebb15339e1ade7157c258b765
Alexey Metelev <alexey.metelev@dekeo.com>
2020-12-30 10:20:17 +0300

    Update postgres client to v11

--- postgresql-client/recipes/install.rb
+++ postgresql-client/recipes/install.rb
@@ -3,41 +3,41 @@ package 'postgresql' do
   action :remove
 end
 
-# Install Postgres 10 Client
-remote_file "#{Chef::Config[:file_cache_path]}/postgresql10-libs.rpm" do
-  source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql10-libs-10.10-1PGDG.rhel6.x86_64.rpm"
+# Install Postgres 11 Client
+remote_file "#{Chef::Config[:file_cache_path]}/postgresql11-libs.rpm" do
+  source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql11-libs-11.9-1PGDG.rhel6.x86_64.rpm"
   action :create
 end
 
-rpm_package "postgresql10-libs-custom" do
-  source "#{Chef::Config[:file_cache_path]}/postgresql10-libs.rpm"
+rpm_package "postgresql11-libs-custom" do
+  source "#{Chef::Config[:file_cache_path]}/postgresql11-libs.rpm"
   action :install
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/postgresql10.rpm" do
-  source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql10-10.10-1PGDG.rhel6.x86_64.rpm"
+remote_file "#{Chef::Config[:file_cache_path]}/postgresql11.rpm" do
+  source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql11-11.9-1PGDG.rhel6.x86_64.rpm"
   action :create
 end
 
-rpm_package "postgresql10-custom" do
-  source "#{Chef::Config[:file_cache_path]}/postgresql10.rpm"
+rpm_package "postgresql11-custom" do
+  source "#{Chef::Config[:file_cache_path]}/postgresql11.rpm"
   action :install
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/postgresql10-devel.rpm" do
-  source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql10-devel-10.10-1PGDG.rhel6.x86_64.rpm"
+remote_file "#{Chef::Config[:file_cache_path]}/postgresql11-devel.rpm" do
+  source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql11-devel-11.9-1PGDG.rhel6.x86_64.rpm"
   action :create
 end
 
-rpm_package "postgresql10-custom" do
-  source "#{Chef::Config[:file_cache_path]}/postgresql10-devel.rpm"
+rpm_package "postgresql11-custom" do
+  source "#{Chef::Config[:file_cache_path]}/postgresql11-devel.rpm"
   action :install
 end
 
-yum_package 'postgresql10' do
+yum_package 'postgresql11' do
   action :install
 end
 
-yum_package 'postgresql10-devel' do
+yum_package 'postgresql11-devel' do
   action :install
 end
