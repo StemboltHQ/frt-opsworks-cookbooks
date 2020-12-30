03964a3d669e258d314f72e887ff8d8314382965
Alexey Metelev <alexey.metelev@dekeo.com>
2020-12-30 10:51:23 +0300

    Refactor postgresql-client recipe
    * Remove confusing "custom" postfix
    * Remove unneeded default actions

--- postgresql-client/recipes/install.rb
+++ postgresql-client/recipes/install.rb
@@ -6,30 +6,24 @@ end
 # Install Postgres 11 Client
 remote_file "#{Chef::Config[:file_cache_path]}/postgresql11-libs.rpm" do
   source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql11-libs-11.9-1PGDG.rhel6.x86_64.rpm"
-  action :create
 end
 
-rpm_package "postgresql11-libs-custom" do
+rpm_package "postgresql11-libs" do
   source "#{Chef::Config[:file_cache_path]}/postgresql11-libs.rpm"
-  action :install
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/postgresql11.rpm" do
   source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql11-11.9-1PGDG.rhel6.x86_64.rpm"
-  action :create
 end
 
-rpm_package "postgresql11-custom" do
+rpm_package "postgresql11" do
   source "#{Chef::Config[:file_cache_path]}/postgresql11.rpm"
-  action :install
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/postgresql11-devel.rpm" do
   source "https://jiffy-linux-packages.s3.amazonaws.com/postgresql11-devel-11.9-1PGDG.rhel6.x86_64.rpm"
-  action :create
 end
 
-rpm_package "postgresql11-custom" do
+rpm_package "postgresql11" do
   source "#{Chef::Config[:file_cache_path]}/postgresql11-devel.rpm"
-  action :install
 end
